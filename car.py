d4a3513aaa74344c2f70c816bff3398c939dcb3e
fordoo <ispeedly@gmail.com>
2015-03-23 20:29:40 +0800

    Fix I/O error, `Stop` operation error,missing direction .

--- car.py
+++ car.py
@@ -8,6 +8,8 @@ class Wheel(object):
 
         gpio.setup(pin1, gpio.OUT)
         gpio.setup(pin2, gpio.OUT)
+        gpio.setup(pin3, gpio.OUT)
+        gpio.setup(pin4, gpio.OUT)
 
         gpio.output(pin3, True)
         gpio.output(pin4, True)
@@ -35,8 +37,8 @@ class Car(object):
     def __init__(self):
         gpio.setmode(gpio.BOARD)
 
-        self.left_wheel = Wheel(16, 18, 22, 29)
-        self.right_wheel = Wheel(13, 15, 7, 11)
+        self.left_wheel = Wheel(13, 15, 7, 11)
+        self.right_wheel = Wheel(16, 18, 22, 29)
 
     def forward(self):
         self.left_wheel.forward()
@@ -56,7 +58,7 @@ class Car(object):
 
     def stop(self):
         self.left_wheel.stop()
-        self.left_wheel.stop()
+        self.right_wheel.stop()
 
     def shutdown(self):
         self.stop()
